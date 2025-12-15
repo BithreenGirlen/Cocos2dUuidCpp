217440513d4a345635c5a65159a9bb5e56a07c09
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2025-12-15 16:16:00 +0900

    Reduce memory allocation.

--- cocos2d_uuid.cpp
+++ cocos2d_uuid.cpp
@@ -1,52 +1,44 @@
 ﻿
 #include "cocos2d_uuid.h"
 
-/*UUID解号*/
+static constexpr const char g_base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+static constexpr const char g_hexDigits[] = "0123456789abcdef";
+
+/* UUID解号 */
 std::string DecodeUuid(const std::string& src)
 {
-	const std::string strBase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-	const char HexDigits[] = "0123456789abcdef";
-
-	std::string strEncoded;
 	size_t nPos = src.find('@');
-	if (nPos != std::string::npos)
-	{
-		strEncoded = src.substr(0, nPos);
-	}
-	else
-	{
-		strEncoded = src;
-	}
-	if (strEncoded.size() != 22)return std::string();
+	if (nPos == std::string::npos)nPos = 0;
+	if (src.size() - nPos < 22)return {};
 
-	std::string strDecoded;
-
+	const char* pSrc = &src[nPos];
+	char sDecoded[31]{};
+	char* pWritten = sDecoded;
 	for (size_t i = 2; i < 22; i += 2)
 	{
-		size_t l = strBase64.find(strEncoded.at(i));
-		size_t r = strBase64.find(strEncoded.at(i + 1));
-		if (l == std::string::npos || r == std::string::npos)
-		{
-			return std::string();
-		}
+		const char* p1 = strchr(g_base64, pSrc[i]);
+		const char* p2 = strchr(g_base64, pSrc[i + 1]);
+		if (p1 == nullptr || p2 == nullptr)return {};
 
-		strDecoded += HexDigits[l >> 2];
-		strDecoded += HexDigits[((l & 3) << 2) | r >> 4];
-		strDecoded += HexDigits[r & 0xF];
+		size_t l = p1 - g_base64;
+		size_t r = p2 - g_base64;
+
+		*pWritten++ = g_hexDigits[l >> 2];
+		*pWritten++ = g_hexDigits[((l & 3) << 2) | r >> 4];
+		*pWritten++ = g_hexDigits[r & 0xF];
 	}
 
 	std::string strResult;
-	strResult.reserve(36);
-	strResult += strEncoded.substr(0, 2);
-	strResult += strDecoded.substr(0, 6);
+	strResult.append(pSrc, 2);
+	strResult.append(&sDecoded[0], 6);
 	strResult += '-';
-	strResult += strDecoded.substr(6, 4);
+	strResult.append(&sDecoded[6], 4);
 	strResult += '-';
-	strResult += strDecoded.substr(10, 4);
+	strResult.append(&sDecoded[10], 4);
 	strResult += '-';
-	strResult += strDecoded.substr(14, 4);
+	strResult.append(&sDecoded[14], 4);
 	strResult += '-';
-	strResult += strDecoded.substr(18);
+	strResult.append(&sDecoded[18]);
 
 	return strResult;
 }
@@ -54,54 +46,36 @@ std::string DecodeUuid(const std::string& src)
 /*UUID編号*/
 std::string EncodeUuid(const std::string& src)
 {
-	const char Base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-	const std::string strHexDigits = "0123456789abcdef";
-
-	std::string strRaw;
 	size_t nPos = src.find('@');
-	if (nPos != std::string::npos)
-	{
-		strRaw = src.substr(0, nPos);
-	}
-	else
-	{
-		strRaw = src;
-	}
-	if (strRaw.size() != 36)return std::string();
+	if (nPos == std::string::npos)nPos = 0;
+	if (src.size() - nPos < 36)return {};
 
-	std::string strTrimmed;
-	size_t nRead = 0;
-	for (;;)
+	char sEncoded[33]{};
+	constexpr const size_t nDecodedSize = sizeof(sEncoded) - 1;
+	char* pWritten = sEncoded;
+	for (size_t nRead = nPos; (pWritten != &sEncoded[nDecodedSize]) && nRead < 36; ++nRead)
 	{
-		nPos = strRaw.substr(nRead).find('-');
-		if (nPos == std::string::npos)
-		{
-			strTrimmed += strRaw.substr(nRead);
-			break;
-		}
-
-		strTrimmed += strRaw.substr(nRead, nPos);
-		nRead += nPos + 1;
+		if (src[nRead] == '-')continue;
+		*pWritten++ = src[nRead];
 	}
 
 	std::string strResult;
-	strResult.reserve(22);
-
-	strResult += strTrimmed.substr(0, 2);
-
-	for (size_t i = 2; i < 32; i += 3)
+	strResult.resize(22);
+	memcpy(&strResult[0], &src[nPos], 2);
+	pWritten = &strResult[2];
+	for (size_t i = 2; i < nDecodedSize; i += 3)
 	{
-		size_t l = strHexDigits.find(strTrimmed.at(i));
-		size_t m = strHexDigits.find(strTrimmed.at(i + 1));
-		size_t r = strHexDigits.find(strTrimmed.at(i + 2));
+		const char* p1 = strchr(g_hexDigits, sEncoded[i]);
+		const char* p2 = strchr(g_hexDigits, sEncoded[i + 1]);
+		const char* p3 = strchr(g_hexDigits, sEncoded[i + 2]);
+		if (p1 == nullptr || p2 == nullptr || p3 == nullptr)return {};
 
-		if (l == std::string::npos || m == std::string::npos || r == std::string::npos)
-		{
-			return std::string();
-		}
+		size_t l = p1 - g_hexDigits;
+		size_t m = p2 - g_hexDigits;
+		size_t r = p3 - g_hexDigits;
 
-		strResult += Base64[(l << 2) | (m >> 2)];
-		strResult += Base64[((m & 3) << 4) | r];
+		*pWritten++ = g_base64[(l << 2) | (m >> 2)];
+		*pWritten++ = g_base64[((m & 3) << 4) | r];
 	}
 
 	return strResult;
